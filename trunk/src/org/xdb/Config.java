9bda524d7d3a0feffd0e7a765884773f4b6e33ae
carsten.binnig@googlemail.com <carsten.binnig@googlemail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2012-08-30 19:40:48 +0000

    Daily snapshot: Added handler for compile server

--- trunk/src/org/xdb/Config.java
+++ trunk/src/org/xdb/Config.java
@@ -7,13 +7,14 @@ import org.xdb.utils.Identifier;
 
 /**
  * XDB central configuration
+ * 
  * @author cbinnig
- *
+ * 
  */
-public class Config implements Serializable{
-	
+public class Config implements Serializable {
+
 	private static final long serialVersionUID = -769530083761951611L;
-	
+
 	public static final String COMPUTE_DRIVER_CLASS = "com.mysql.jdbc.Driver";
 	public static final String COMPUTE_DB_URL = "jdbc:mysql://127.0.0.1/";
 	public static final String COMPUTE_DB_NAME = "xdb_tmp";
@@ -22,7 +23,7 @@ public class Config implements Serializable{
 	public static final Identifier COMPUTE_NOOP_ID = new Identifier("NOOP");
 	public static final int COMPUTE_PORT = 55555;
 	public static final Integer COMPUTE_MAX_FETCHSIZE = Integer.MAX_VALUE;
-	
+
 	public static final String METADATA_DRIVER_CLASS = "com.mysql.jdbc.Driver";
 	public static final String METADATA_DB_URL = "jdbc:mysql://127.0.0.1/xdb_schema";
 	public static final String METADATA_SCHEMA = "xdb_schema";
@@ -31,9 +32,9 @@ public class Config implements Serializable{
 	public static final String METADATA_OID_NAME = "OID";
 	public static final int METADATA_PORT = 55556;
 	public static final String XDB_DEFAULT_SCHEMA = "PUBLIC";
-	
+
 	public static final int QUERYTRACKER_PORT = 55557;
-	
+
 	public static final String LOG_FILE = "./log/xdb.log";
 	public static final Level LOG_LEVEL = Level.INFO;
 }
