43cc5d5f65e5faec39c0dbe94cd912bcab547147
carsten.binnig <carsten.binnig@googlemail.com>
2012-08-30 19:43:00 +0000

    Daily snapshot: Added handler for compile server

---
+++ src/org/xdb/store/MySQLStore.java
@@ -0,0 +1,31 @@
+package org.xdb.store;
+
+import org.xdb.funsql.compile.operator.AbstractOperator;
+import org.xdb.funsql.compile.operator.EnumOperator;
+import org.xdb.store.JDBCStore;
+
+public class MySQLStore extends JDBCStore {
+	private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+
+	public MySQLStore() {
+		super(DRIVER_CLASS);
+	}
+
+	@Override
+	public boolean supportsOperator(EnumOperator operator) {
+		switch (operator) {
+		case EQUI_SELECTION:
+		case SIMPLE_PROJECTION:
+		case EQUI_JOIN:
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	@Override
+	public String generateQuery(AbstractOperator operator) {
+		return null;
+	}
+
+}
