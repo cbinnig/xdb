6d32e168c9b75272a5f5fdb60e86065149c3292a
carsten.binnig <carsten.binnig@googlemail.com>
2012-12-27 15:45:14 +0000

    Added SQL for testing

---
+++ sql/test.sql
@@ -0,0 +1,75 @@
+-- MySQL dump 10.13  Distrib 5.6.5-m8, for osx10.6 (i386)
+--
+-- Host: localhost    Database: xdb_test
+-- ------------------------------------------------------
+-- Server version	5.6.5-m8
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `r`
+--
+
+DROP TABLE IF EXISTS `r`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `r` (
+  `a` int(11) DEFAULT NULL,
+  `b` text,
+  `c` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `r`
+--
+
+LOCK TABLES `r` WRITE;
+/*!40000 ALTER TABLE `r` DISABLE KEYS */;
+INSERT INTO `r` VALUES (1,'Test',1);
+/*!40000 ALTER TABLE `r` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `s`
+--
+
+DROP TABLE IF EXISTS `s`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `s` (
+  `d` int(11) DEFAULT NULL,
+  `e` text,
+  `f` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `s`
+--
+
+LOCK TABLES `s` WRITE;
+/*!40000 ALTER TABLE `s` DISABLE KEYS */;
+INSERT INTO `s` VALUES (1,'Test',1);
+/*!40000 ALTER TABLE `s` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-12-27 16:44:48
