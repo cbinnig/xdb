de5210f1d88fc59fd1dbcff09395465f039421e4
carsten.binnig@googlemail.com <carsten.binnig@googlemail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2013-03-11 19:55:11 +0000

    Made test configurable to run locally or distributed

--- trunk/src/org/xdb/test/DistributedQueryTrackerTestCase.java
+++ trunk/src/org/xdb/test/DistributedQueryTrackerTestCase.java
@@ -13,48 +13,65 @@ public class DistributedQueryTrackerTestCase extends TestCase {
 	protected ComputeServer[] computeServers;
 	protected QueryTrackerServer qServer;
 	private int numberOfSlots;
-	
-	public DistributedQueryTrackerTestCase(int numberOfComputeServer) {
-		this(numberOfComputeServer, 1);
+	private boolean runLocal;
+
+	public DistributedQueryTrackerTestCase(int numberOfComputeServer,
+			boolean runLocal) {
+		this(numberOfComputeServer, 1, runLocal);
 	}
-	
-	public DistributedQueryTrackerTestCase(int numberOfComputeServer, int numberOfSlots) {
+
+	public DistributedQueryTrackerTestCase(int numberOfComputeServer,
+			int numberOfSlots, boolean runLocal) {
 		super();
 		this.computeServers = new ComputeServer[numberOfComputeServer];
 		this.numberOfSlots = numberOfSlots;
+		this.runLocal = runLocal;
 	}
-	
+
 	@Override
-	public void setUp(){
-		
+	public void setUp() {
+
 		assertNoError(CompileServer.deleteCatalog());
 
 		try {
 			mTrackerServer = new MasterTrackerServer();
 			mTrackerServer.startServer();
 			assertNoError(mTrackerServer.getError());
-			
+
 			qServer = new QueryTrackerServer();
 			qServer.startServer();
 			assertNoError(qServer.getError());
-			
-			for(int i=0; i<this.computeServers.length; ++i){
-				computeServers[i] = new ComputeServer(Config.COMPUTE_PORT+i, numberOfSlots);
-				computeServers[i].startServer();
-				assertNoError(computeServers[i].getError());
+
+			if (this.runLocal) {
+				for (int i = 0; i < this.computeServers.length; ++i) {
+					computeServers[i] = new ComputeServer(Config.COMPUTE_PORT
+							+ i, numberOfSlots);
+					computeServers[i].startServer();
+					assertNoError(computeServers[i].getError());
+				}
+			}
+			else{
+				System.out.print("Waiting for query tracker server ...");
+				while(this.mTrackerServer.getNoFreeQueryTrackerSlots()<computeServers.length){
+					System.out.print(".");
+					Thread.sleep(1000);
+				}
+				System.out.println();
 			}
 
 		} catch (Exception e) {
 			Assert.assertTrue(e.toString(), false);
 		}
 	}
-	
+
 	@Override
-	public void tearDown(){
+	public void tearDown() {
 		mTrackerServer.stopServer();
 		qServer.stopServer();
-		for(int i=0; i<this.computeServers.length; ++i){
-			this.computeServers[i].stopServer();
+		if (this.runLocal) {
+			for (int i = 0; i < this.computeServers.length; ++i) {
+				this.computeServers[i].stopServer();
+			}
 		}
 	}
 }
