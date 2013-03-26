588ce959f7906081a1cceae076b930c11db40c55
carsten.binnig <carsten.binnig@googlemail.com>
2013-03-26 17:15:13 +0000

    Fixed eclipse project setup for hive test cases

--- examples/tpch/tpch_examples.sql
+++ examples/tpch/tpch_examples.sql
@@ -9,6 +9,7 @@ order by frequency desc;
 -- basket analysis with UDF (simlarity of p_type)
 
 -- step 1: find products in same order
+select count(*) from (
 select l1.l_partkey as p1_key, l2.l_partkey as p2_key, 
 p1.p_type as p1_type, p2.p_type as p2_type, count(*) as frequency
 from lineitem l1, lineitem l2, part p1, part p2
@@ -16,7 +17,42 @@ where l1.l_orderkey = l2.l_orderkey
 and l1.l_partkey = p1.p_partkey 
 and l2.l_partkey = p2.p_partkey 
 and l1.l_partkey != l2.l_partkey
+and l1.l_shipdate > date '1998-03-15' 
+and l2.l_shipdate > date '1998-03-15'
 group by l1.l_partkey, l2.l_partkey, p1.p_type, p2.p_type
-order by frequency desc;
+having count(*)>=2
+order by frequency desc
+) as temp;
 
 -- step 2: UDF to filter products with similar types
+
+-- explain plan
+
+explain select l1.l_partkey as p1_key, l2.l_partkey as p2_key, 
+p1.p_type as p1_type, p2.p_type as p2_type, count(*) as frequency
+from lineitem l1, lineitem l2, part p1, part p2
+where l1.l_orderkey = l2.l_orderkey
+and l1.l_partkey = p1.p_partkey 
+and l2.l_partkey = p2.p_partkey 
+and l1.l_partkey != l2.l_partkey
+and l1.l_shipdate > date '1998-03-15' 
+and l2.l_shipdate > date '1998-03-15' 
+group by l1.l_partkey, l2.l_partkey, p1.p_type, p2.p_type
+having count(*)>=2
+order by frequency desc;
+
+explain select l1.l_partkey as p1_key, l2.l_partkey as p2_key
+from lineitem l1, lineitem l2
+where l1.l_orderkey = l2.l_orderkey
+and l1.l_partkey != l2.l_partkey
+and l1.l_shipdate > date '1998-03-15' 
+and l2.l_shipdate > date '1998-03-15';
+
+select count(*) from (
+select l1.l_partkey as p1_key, l2.l_partkey as p2_key
+from lineitem l1, lineitem l2
+where l1.l_orderkey = l2.l_orderkey
+and l1.l_partkey != l2.l_partkey
+and l1.l_shipdate > date '1998-03-15' 
+and l2.l_shipdate > date '1998-03-15'
+) as temp;
