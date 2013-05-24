a7938f4b4f1849c4ff3b0527550828bacf00e7b5
carsten.binnig@googlemail.com <carsten.binnig@googlemail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2013-05-24 14:14:26 +0000

    Added TPCH Q15

--- trunk/src/org/xdb/test/tpch/TestTPCH.java
+++ trunk/src/org/xdb/test/tpch/TestTPCH.java
@@ -1,6 +1,5 @@
 package org.xdb.test.tpch;
 
-import org.junit.Test;
 import org.xdb.client.CompileClient;
 import org.xdb.error.Error;
 import org.xdb.test.XDBTestCase;
@@ -118,7 +117,30 @@ public class TestTPCH extends XDBTestCase {
 		this.assertNoError(error);
 	}
 	
-	@Test
+
+	/*
+select
+	l_returnflag,
+	l_linestatus,
+	sum(l_quantity) as sum_qty,
+	sum(l_extendedprice) as sum_base_price,
+	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
+	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
+	avg(l_quantity) as avg_qty,
+	avg(l_extendedprice) as avg_price,
+	avg(l_discount) as avg_disc,
+	count(*) as count_order
+from
+	lineitem
+where
+	l_shipdate <= date '1998-12-01' - interval ':1' day (3)
+group by
+	l_returnflag,
+	l_linestatus
+order by
+	l_returnflag,
+	l_linestatus;
+	 */
 	public void testQ1(){
 		String q1 = 
 				"select	l_returnflag,	" +
@@ -140,7 +162,49 @@ public class TestTPCH extends XDBTestCase {
 	
 	public void testQ2(){
 /*
-
+select
+	s_acctbal,
+	s_name,
+	n_name,
+	p_partkey,
+	p_mfgr,
+	s_address,
+	s_phone,
+	s_comment
+from
+	part,
+	supplier,
+	partsupp,
+	nation,
+	region
+where
+	p_partkey = ps_partkey
+	and s_suppkey = ps_suppkey
+	and p_size = :1
+	and p_type like '%:2'
+	and s_nationkey = n_nationkey
+	and n_regionkey = r_regionkey
+	and r_name = ':3'
+	and ps_supplycost = (
+		select
+			min(ps_supplycost)
+		from
+			partsupp,
+			supplier,
+			nation,
+			region
+		where
+			p_partkey = ps_partkey
+			and s_suppkey = ps_suppkey
+			and s_nationkey = n_nationkey
+			and n_regionkey = r_regionkey
+			and r_name = ':3'
+	)
+order by
+	s_acctbal desc,
+	n_name,
+	s_name,
+	p_partkey;
  */
 		String createQ2 = "CREATE FUNCTION q2( OUT o1 TABLE) \n" +
 				"BEGIN \n" +
@@ -341,12 +405,56 @@ group by
 		this.executeStmt(q10);
 	}
 	
+	public void testQ15(){
+		
+		String createQ15 = 
+				"CREATE FUNCTION q15( OUT o1 TABLE) \n" +
+				"BEGIN \n" +
+				"  :t1 = " +
+				"		select " +
+				"			l_suppkey as supply_no, " +
+				"			sum(l_extendedprice * (1 - l_discount)) as total_revenue " +
+				"		from " +
+				"			lineitem" +
+				"		where " +
+				"			l_shipdate >= date '1996-01-01' and l_shipdate < date '1996-04-01' " +
+				"		group by  " +
+				"			l_suppkey;"+ 
+				""+
+				"	:t2 = " +
+				"		select " +
+				"			max(total_revenue) as max_total_revenue " +
+				"		from :t1; " +
+				""+
+				"	:o1 = " +
+				"		select " +
+				"			s_suppkey, " + 
+				"       	s_name, " + 
+				"      	 	s_address, " + 
+				"       	s_phone, " + 
+				"       	total_revenue " +
+				"		from " +
+				"			supplier," +
+				"			:t1 as t1, " +
+				"			:t2 as t2 " +
+				"		where" +
+				"			s_suppkey = supply_no " +
+				"			and max_total_revenue = total_revenue;" +
+				"END;";
+		
+		String callQ15 = "CALL FUNCTION q15;";
+		
+		executeStmt(createQ15);
+		executeStmt(callQ15);
+	}
+	
 	public void testSimpleAggregation(){
 		String test = "Select l_suppkey " +
 					"from lineitem " +
 					"group by L_SUPPKEY;";
 		this.executeStmt(test);
 	}
+	
 	@Override
 	public void tearDown(){
 		super.tearDown();
