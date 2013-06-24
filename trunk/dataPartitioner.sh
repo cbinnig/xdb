89829f95b03da93944f3021c93f18ec52794dc73
carsten.binnig@googlemail.com <carsten.binnig@googlemail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2013-06-24 16:39:42 +0000

    Run data partitioner from jar

--- trunk/dataPartitioner.sh
+++ trunk/dataPartitioner.sh
@@ -1,3 +1,1 @@
-java -Xms2048M -Xmx4096M -cp ./bin/:./lib/commons-cli-1.2.jar org.xdb.tools.partitioner.DataPartitioner $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21}
-
-
+java -cp ./XDB_executable.jar:./lib/commons-cli-1.2.jar org.xdb.tools.partitioner.DataPartitioner $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
