335f6fe657301c6593dc384ec35ca92aa428fdf8
eng.asalameh@gmail.com <eng.asalameh@gmail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2013-06-28 15:48:59 +0000

    Added minor change to the partitioner script.

--- trunk/dataPartitioner.sh
+++ trunk/dataPartitioner.sh
@@ -1,1 +1,1 @@
-java -cp ./XDB_executable.jar:./lib/commons-cli-1.2.jar org.xdb.tools.partitioner.DataPartitioner $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13
+java -cp ./XDB_executable.jar:./lib/commons-cli-1.2.jar org.xdb.tools.partitioner.DataPartitioner $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13}
