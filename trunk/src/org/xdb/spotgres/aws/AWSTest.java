36b855ddbcf7331b97143a50adff10ca740899bf
sven.listing@teamaol.com <sven.listing@teamaol.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2013-08-06 14:47:14 +0000

    Calculate the availabiltity based on a given max bid. Also calculates a weighted average and the min & max pricetag for the investigated timeframe.

--- trunk/src/org/xdb/spotgres/aws/AWSTest.java
+++ trunk/src/org/xdb/spotgres/aws/AWSTest.java
@@ -20,30 +20,32 @@ import com.amazonaws.services.ec2.model.SpotPrice;
 
 @SuppressWarnings("unused")
 public class AWSTest {
-    private AmazonEC2      ec2;
-    private ArrayList<String> instanceIds;
-    private ArrayList<String> spotInstanceRequestIds;
+	private AmazonEC2 ec2;
+	private ArrayList<String> instanceIds;
+	private ArrayList<String> spotInstanceRequestIds;
+
 	@Before
 	public void setUp() throws Exception {
-        AWSCredentials credentials = new PropertiesCredentials(AWSTest.class.getResourceAsStream("AwsCredentials.properties"));
+		AWSCredentials credentials = new PropertiesCredentials(
+				AWSTest.class.getResourceAsStream("AwsCredentials.properties"));
 
-        ec2 = new AmazonEC2Client(credentials);
+		ec2 = new AmazonEC2Client(credentials);
 		Region euWest1 = Region.getRegion(Regions.EU_WEST_1);
-		
+
 		ec2.setRegion(euWest1);
 	}
 
 	@Test
 	public void test() {
 		DescribeSpotPriceHistoryRequest sphr = new DescribeSpotPriceHistoryRequest();
-		//sphr.setAvailabilityZone(Region.getRegion(Regions.EU_WEST_1).getName());
+		// sphr.setAvailabilityZone(Region.getRegion(Regions.EU_WEST_1).getName());
 		Collection<String> types = new ArrayList<String>();
 		types.add(InstanceType.M1Large.toString());
 		sphr.setInstanceTypes(types);
 		DescribeSpotPriceHistoryResult result = ec2.describeSpotPriceHistory(sphr);
 		List<SpotPrice> prices = result.getSpotPriceHistory();
-		for(SpotPrice price:prices){
-			System.out.println(price.getTimestamp() + ": "+ price.getSpotPrice());
+		for (SpotPrice price : prices) {
+			System.out.println(price.getTimestamp() + ": " + price.getSpotPrice());
 		}
 	}
 
