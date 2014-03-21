52117106087ec37718aa4bb500ba845ad8022e03
carsten.binnig@googlemail.com <carsten.binnig@googlemail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2014-03-21 14:59:11 +0000

    Adopted shell scripts for remote testing

--- trunk/computeServer.sh
+++ trunk/computeServer.sh
@@ -1,1 +1,4 @@
-java -cp ./bin/:./lib/mysql-connector-java-5.1.20-bin.jar org.xdb.server.ComputeServer $1 $2
+java -cp ./bin/:\
+./lib/mysql-connector-java-5.1.20-bin.jar:\
+./lib/xdb.jar \
+org.xdb.server.ComputeServer $1 $2
--- trunk/masterTrackerServer.sh
+++ trunk/masterTrackerServer.sh
@@ -1,5 +1,6 @@
 java -cp ./bin/:\
 ./lib/mysql-connector-java-5.1.20-bin.jar:\
 ./lib/antlr-3.3-complete.jar:\
-./lib/oy-lm-1.4.jar \
+./lib/oy-lm-1.4.jar:\
+./lib/xdb.jar \
 org.xdb.server.MasterTrackerServer
