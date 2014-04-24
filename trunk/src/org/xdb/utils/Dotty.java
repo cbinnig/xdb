c8e6e027ef6574bd80df6e38cb3ea8266274b8de
carsten.binnig@googlemail.com <carsten.binnig@googlemail.com@d64771a4-391e-8a18-7e7c-73b59b0fcb41>
2014-04-24 12:34:52 +0000

    Dotty fix for Mac

--- trunk/src/org/xdb/utils/Dotty.java
+++ trunk/src/org/xdb/utils/Dotty.java
@@ -1,7 +1,9 @@
 package org.xdb.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.xdb.Config;
 
@@ -9,17 +11,31 @@ import com.oy.shared.lm.graph.Graph;
 import com.oy.shared.lm.out.GRAPHtoDOTtoGIF;
 
 public class Dotty {
-	public static String dot2Img(Graph graph, String fileName){
+	public static String dot2Img(Graph graph, String fileName) {
 		final String path = Config.DOT_TRACE_PATH;
-		final String dotFileName = new File(path, fileName + ".dot").getAbsolutePath(); 
-		final String gifFileName =  new File(path, fileName + ".gif").getAbsolutePath();
+		final String dotFileName = new File(path, fileName + ".dot")
+				.getAbsolutePath();
+		final String gifFileName = new File(path, fileName + ".gif")
+				.getAbsolutePath();
 		final String exeFileName = Config.DOT_EXE;
+
 		try {
-			GRAPHtoDOTtoGIF.transform(graph, dotFileName, gifFileName, exeFileName);
+			GRAPHtoDOTtoGIF.transform(graph, dotFileName, gifFileName,
+					exeFileName);
 		} catch (final IOException e) {
 			// do nothing
 		}
-		
+
+		if (Config.PLATTFORM.equals("MAC")) {
+			final String cmd = exeFileName+" -Tgif "+dotFileName+" -o "+gifFileName;
+			try {
+				Process p = Runtime.getRuntime().exec(cmd);
+				p.waitFor();
+			} catch (Exception e) {
+				// do nothing
+			}
+		}
+
 		return gifFileName;
 	}
 }
