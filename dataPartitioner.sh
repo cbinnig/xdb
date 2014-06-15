c7a4360c6cde4c466a6be36ebaacb65b2cbd5b93
carsten.binnig <carsten.binnig@googlemail.com>
2014-06-15 22:40:41 +0000

    DoomDB: scripts and config

--- dataPartitioner.sh
+++ dataPartitioner.sh
@@ -1,1 +1,1 @@
-java -Xms4096m -Xmx6144m -cp ./XDB_executable.jar:./lib/commons-cli-1.2.jar org.xdb.tools.partitioner.DataPartitioner $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+java -Xms4096m -Xmx6144m -cp ./lib/xdb.jar:./lib/commons-cli-1.2.jar org.xdb.tools.partitioner.DataPartitioner $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
